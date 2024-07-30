9fa42e682f40bbcaa1bd8bc3194f0c849ca5f703
Romulo de Moraes <romulotg12@gmail.com>
2024-07-30 14:08:36 -0300

    feat: methods to register and login on Client

--- client/internal/ChatClient.py
+++ client/internal/ChatClient.py
@@ -6,6 +6,8 @@ class ChatClient:
         self.host = host
         self.port = port
         self.client_socket = socket(AF_INET, SOCK_STREAM)
+        self.user = None
+        self.state = None
 
     def start(self):
         self.client_socket.connect((self.host, self.port))
@@ -51,9 +53,58 @@ class ChatClient:
                 print(f"An error occurred: {e}")
                 break
 
+    def handle_auth(self):
+        while True:
+            message = input("You want login or register? ")
+
+            if message == "2":
+                name = input("Please enter your name: ")
+                self.client_socket.send(f"01{name}")
+                return True
+
+            elif message == "1":
+                while True:
+                    credentials = input("Please enter your id and name (separated by space): ")
+                    parts = credentials.split()
+                    if len(parts) != 2:
+                        print("Unknown command.")
+                    
+                    id, name = parts
+
+                    try:
+                        id = int(id)
+                        if len(id) == 13 :
+                            break
+                        else: 
+                            print("Invalid ID (must be have 13 digits).")
+                    except ValueError:
+                        print("Invalid ID.")
+
+                self.client_socket.send(f"03{id}{name}")
+                return True
+
+            else:
+                print("Unknow command.")
+
+
     def handle_user_input(self):
         try:
             while True:
+                if not self.user:
+                    print("You need to autenticate to enter on Interzap:\n")
+                    print("Type 1 to login.\nor\nType 2 to register.")
+                    auth = self.handle_auth()
+                    if auth:
+                        print("Message sended to server. Waiting for response...")
+
+                elif self.user and self.state == None:
+                    print("What do you wanna do now on Interzap?")
+                    print("1 - Add new contact")
+                    print("2 - List my contacts to read and send messages")
+                    print("3 - Quit")
+                    
+
+
                 message = input()
                 self.client_socket.send(message.encode('utf-8'))
         except Exception as e:
