778ce7e06e93ef56f8170a8aad1e7f12a5e87be0
Romulo de Moraes <romulotg12@gmail.com>
2024-07-30 13:38:48 -0300

    feat: adding thread to handle user input on client

--- client/internal/ChatClient.py
+++ client/internal/ChatClient.py
@@ -1,4 +1,5 @@
 from socket import *
+import threading
 
 class ChatClient:
     def __init__(self, host, port):
@@ -29,13 +30,35 @@ class ChatClient:
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠒⠨⠥⢀⣈⣃⣈⣚⣀⠤⠅⠒⠉⠀⠀ 
 
 """)
-        self.handle_messages()
+        receive_thread = threading.Thread(target=self.handle_messages)
+        receive_thread.start()
+
+        self.handle_user_input()
 
     def close(self):
         self.client_socket.close()
 
     def handle_messages(self):
         while True:
-            data = self.client_socket.recv(1024)
-            print(f"{data}")
+            try:
+                data = self.client_socket.recv(1024)
+                if data:
+                    print(data.decode('utf-8'))
+                else:
+                    print("Connection closed by the server.")
+                    break
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                break
+
+    def handle_user_input(self):
+        try:
+            while True:
+                message = input()
+                self.client_socket.send(message.encode('utf-8'))
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            self.close()
+
 
