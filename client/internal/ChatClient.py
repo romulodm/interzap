bf22062fa80aca4fa325643ca1a9a612ff9aaecf
Romulo <romulotg12@gmail.com>
2024-08-01 20:53:20 -0300

    fix: use of wrong variable in login attempt response

--- client/internal/ChatClient.py
+++ client/internal/ChatClient.py
@@ -10,6 +10,120 @@ class ChatClient:
         self.client_socket = socket(AF_INET, SOCK_STREAM)
         self.client = None
         self.state = None
+        self.alive = True
+
+    def close(self):
+        self.alive = False
+        self.client_socket.close()
+
+    def handle_messages(self):
+        while self.alive:
+            try:
+                message = self.client_socket.recv(1024).decode('utf-8')
+                if message == "Welcome to Interzap!":
+                    print(f"{message}\n")
+                
+                elif message[:2] == "02":
+                    id = message[2:]
+                    self.client = Client(id, self.client_socket)
+                    print(f"Authenticated with id: {id}\n")
+                
+                elif message[:2] == "04":
+                    response = message[2:]
+                    if response == "Error":
+                        print("An error occurred with your login.")
+
+                    else: 
+                        self.client = Client(response, self.client_socket)
+                        print(f"Authenticated with id: {response}\n")              
+                    
+            except Exception as e:
+                print(f"An error occurred on handler a message received: {e}")
+                self.close()
+                break
+
+    def handle_auth(self):
+        while self.alive and not self.client:
+            message = input("You want login or register? ")
+
+            if message == "1":
+                self.client_socket.send(b'01')
+                print("Processing your registration request...")
+                return True
+
+            elif message == "2":
+                while self.alive:
+                    id = input('Please enter your id (type "n" to cancel): ')
+                    if id == "n" or id == "N":
+                        print()
+                        break
+                    if len(id) == 13:
+                        self.client_socket.send(f"03{id}".encode("utf-8"))
+                        print("Processing your login request...")
+                        time.sleep(0.5)
+                        break
+                    else: 
+                        print("Invalid ID - must be have 13 digits.")
+
+                return True
+
+            else:
+                print("Unknow command.")
+
+    def handle_state(self):
+        while self.alive and self.client:
+            message = input("What do you wanna do now on Interzap? ")
+
+            if message == "1":
+                self.state == "NEW_CONTACT"
+                break
+
+            if message == "2":
+                self.state == "NEW_GROUP"
+                break
+
+            elif message == "3":
+                self.state == "LIST_CONTACTS_AND_GROUPS"
+                break
+
+            elif message == "4":
+                self.close()
+                break
+
+            else:
+                print("Unknow command.")
+
+
+    def handle_user_input(self):
+        try:
+            while self.alive:
+                if not self.client:
+                    print("You need to autenticate to enter on Interzap:\n")
+                    print("Type 1 to register.\nor\nType 2 to login.")
+                    self.handle_auth()
+    
+                elif self.client and self.state == None:
+                    print("List of possibles commands:")
+                    print("1 - Add new contact")
+                    print("2 - Create new group")
+                    print("3 - List my contacts and groups to read and send messages")
+                    print("4 - Quit")
+                    self.handle_state()
+
+                elif self.client and self.state == "NEW_CONTACT":
+                    pass
+
+                elif self.client and self.state == "NEW_GROUP":
+                    pass
+
+                elif self.client and self.state == "LIST_CONTACTS_AND_GROUPS":
+                    pass
+
+                
+        except Exception as e:
+            print(f"An error occurred: {e}")
+        finally:
+            self.close()
 
     def start(self):
         try:
@@ -43,111 +157,4 @@ Connected on Interzap 1.0 © 2024
         
         except Exception as e:
             print(f"An error occurred when trying to connect to the server: {e}")
-
-    def close(self):
-        self.client_socket.close()
-
-    def handle_messages(self):
-        while True:
-            try:
-                message = self.client_socket.recv(1024).decode('utf-8')
-                if message == "Welcome to Interzap!":
-                    print(f"{message}\n")
-                
-                elif message[:2] == "02":
-                    id = message[2:]
-                    self.client = Client(id, self.client_socket)
-                    print(f"Authenticated with id: {id}\n")
-                
-                elif message[:2] == "04":
-                    response = message[2:]
-                    if response == "Error":
-                        print("An error occurred with your login.")
-                    else: 
-                        self.client = Client(id, self.client_socket)
-                        print(f"Authenticated with id: {id}\n")              
-                    
-            except Exception as e:
-                print(f"An error occurred on handler a message received: {e}")
-                break
-
-    def handle_auth(self):
-        while True:
-            message = input("You want login or register? ")
-
-            if message == "1":
-                self.client_socket.send(b'01')
-                print("Processing your registration request...")
-                return True
-
-            elif message == "2":
-                while True:
-                    id = input("Please enter your id: ")
-                    if len(id) == 13 :
-                        break
-                    else: 
-                        print("Invalid ID - must be have 13 digits.")
-
-                self.client_socket.send(f"03{id}".encode("utf-8"))
-                print("Processing your login request...")
-                return True
-
-            else:
-                print("Unknow command.")
-
-    def handle_state(self):
-        while True:
-            message = input("What do you wanna do? ")
-
-            if message == "1":
-                self.state == "NEW_CONTACT"
-                break
-
-            if message == "2":
-                self.state == "NEW_GROUP"
-                break
-
-            elif message == "3":
-                self.state == "LIST_CONTACTS_AND_GROUPS"
-                break
-
-            elif message == "4":
-                self.close()
-                break
-
-            else:
-                print("Unknow command.")
-
-
-    def handle_user_input(self):
-        try:
-            while True:
-                if not self.client:
-                    print("You need to autenticate to enter on Interzap:\n")
-                    print("Type 1 to register.\nor\nType 2 to login.")
-                    self.handle_auth()
-    
-                elif self.client and self.state == None:
-                    print("What do you wanna do now on Interzap?")
-                    print("1 - Add new contact")
-                    print("2 - Create new group")
-                    print("3 - List my contacts and groups to read and send messages")
-                    print("4 - Quit")
-                    self.handle_auth()
-
-                elif self.client and self.state == "NEW_CONTACT":
-                    pass
-
-                elif self.client and self.state == "NEW_GROUP":
-                    pass
-
-                elif self.client and self.state == "LIST_CONTACTS_AND_GROUPS":
-                    pass
-
-                
-        except Exception as e:
-            print(f"An error occurred: {e}")
-        finally:
-            self.close()
-
 
