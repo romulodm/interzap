03d9175210699ee28a17b1507b8015f363a82360
Romulo <romulotg12@gmail.com>
2024-07-31 23:12:41 -0300

    feat: offline users and users_counter added

--- server/internal/ChatServer.py
+++ server/internal/ChatServer.py
@@ -9,7 +9,9 @@ class ChatServer:
         self.host = host
         self.port = port
         self.server_socket = socket(AF_INET, SOCK_STREAM)
-        self.users = {}
+        self.users_counter = 0
+        self.online_users = {}
+        self.offline_users = {}
         self.unauthenticated_users = {}
 
     def close(self):
@@ -34,20 +36,16 @@ class ChatServer:
         self.unauthenticated_users[addr] = user
 
     def register_user(self, addr, id, user):
-        self.users[id] = user
+        self.online_users[id] = user
+        self.users_count += 1
 
         # Removing user from without authentication ones by your addr
         if addr in self.unauthenticated_users:
             del self.unauthenticated_users[addr]
 
     def unregister_user(self, id):
-        if id in self.users:
-            del self.users[id]
-
-    def send_message(self, id, message):
-        if id in self.users:
-            self.users[id].conn.sendall(f'{message}'.encode('utf-8'))
-        else:
-            if id not in self.users:
-                self.users[id].messages.insert(message)
+        user = self.online_users[id]
+        if user:
+            del self.online_users[id]
+            self.offline_users[id] = user
             
