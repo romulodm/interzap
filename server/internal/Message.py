c7d9fd38b4c5ce4048591ca7af8f79e0c7a9a76a
Romulo <romulotg12@gmail.com>
2024-08-02 00:02:59 -0300

    feat: adding Message class

---
+++ server/internal/Message.py
@@ -0,0 +1,21 @@
+class Message:
+    def __init__(self, sender_id, receiver_id, time, content):
+        self.sender_id = sender_id
+        self.receiver_id = receiver_id
+        self.time = time
+        self.content = content
+        self.read = None
+        self.delivered = None
+
+    def make_delivered(self):
+        self.delivered = True
+
+    def is_delivered(self):
+        return self.delivered
+    
+    def make_read(self):
+        self.read = True
+
+    def is_read(self):
+        return self.read
+    
