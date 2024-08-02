9f13ba19285c6c0c0477533789d00f82cc9f87c3
Romulo <romulotg12@gmail.com>
2024-08-02 14:18:51 -0300

    feat: adding Group class on server

---
+++ server/internal/Group.py
@@ -0,0 +1,21 @@
+import time
+from typing import List
+from internal.User import User
+from internal.Message import Message
+
+class Group:
+    def __init__(self, server, users):
+        self.server = server
+        self.id = self.generate_group_id()
+        self.creation = str(time.time())[:10]
+        self.users: List[User] = users
+
+    def generate_group_id(self):
+        return f'Group-{self.server.groups_counter + 1:07d}'
+    
+    def send_message(self, message: Message):
+        for user in self.users:
+            if user.id in self.server.online_users:
+                user.conn.sendall(f"12{message.sender_id}{self.id}{message.time}{message.content}".encode("utf-8"))
+            else:
+                user.messages.insert(message)
