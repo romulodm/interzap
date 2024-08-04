ad323cb19a8707e49777425a58e423e37d1e69c5
Romulo <romulotg12@gmail.com>
2024-08-04 12:28:13 -0300

    feat: adding methods to save and load messages in a json file (to serve as a backup for clients)

--- client/internal/Client.py
+++ client/internal/Client.py
@@ -1,11 +1,64 @@
+import json
+import os
+import time
+
 class Client:
-    def __init__(self, id, socket):
+    def __init__(self, id):
         self.id = id
         self.contacts = []
+        self.messages = {}
 
-    def add_contact(self, user):
-        if user.id != self.id:
-            self.contacts.append(user)
+    def add_contact(self, id):
+        if id not in self.contacts:
+            self.contacts.append(id)
+        else:
+            print("You have already added this contact.")
 
-    def remove_contact(self, user):
-        self.contacts = [contact for contact in self.contacts if contact.id != user.id]
+    def remove_contact(self, id):
+        if id in self.contacts:
+            self.contacts.remove(id)
+        else:
+            print("Contact does not exist.")
+
+    def add_message(self, sender_id, receiver_id, content, timestamp=None):
+        if timestamp is None:
+            timestamp = int(time.time())
+        
+        message_data = {
+            'sender': sender_id,
+            'time': timestamp,
+            'content': content
+        }
+
+        if sender_id == self.id:
+            if receiver_id not in self.messages:
+                self.messages[receiver_id] = []
+            self.messages[receiver_id].append(message_data)
+            self.messages[receiver_id].sort(key=lambda x: x['time'])
+        
+        if receiver_id == self.id:
+            if sender_id not in self.messages:
+                self.messages[sender_id] = []
+            self.messages[sender_id].append(message_data)
+            self.messages[sender_id].sort(key=lambda x: x['time'])
+
+    def save_data_to_file(self):
+        filename = f"client/backups/{self.id}.json"
+        data = {
+            'contacts': self.contacts,
+            'messages': self.messages
+        }
+        with open(filename, 'w') as file:
+            json.dump(data, file, indent=4)
+
+    def load_data_from_file(self):
+        filename = f"client/backups/{self.id}.json"
+        if os.path.exists(filename):
+            with open(filename, 'r') as file:
+                data = json.load(file)
+                self.contacts = data.get('contacts', [])
+                self.messages = data.get('messages', {})
+        else:
+            self.contacts = []
+            self.messages = {}
+            
