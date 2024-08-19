449bc88aa1bb5974662289af0a5b7da90c3c77fe
Romulo <romulotg12@gmail.com>
2024-08-19 08:19:44 -0300

    feat: using sqlite to save pending messages from a user offline

--- server/internal/chat_server.py
+++ server/internal/chat_server.py
@@ -3,7 +3,6 @@ import time
 from socket import *
 from typing import Dict
 from internal.user import User
-from internal.message import Message
 from internal.group import Group
 from internal.database import Database
 
@@ -46,6 +45,9 @@ class ChatServer:
             member_ids = [member[0] for member in members]
             self.groups[group_id] = Group(group_id, None, member_ids)
 
+    def load_pending_messages(self, id):
+        return self.db.get_pending_messages(id)
+ 
     def start(self):
         try:
             self.load_users_from_db()
@@ -69,10 +71,11 @@ class ChatServer:
             thread = threading.Thread(target=user.start)
             thread.start()
 
-    def register_user(self, addr, id, user: User):
+    def register_user(self, id, user: User):
         try:
             self.online_users[id] = user
             self.users_counter += 1
+            user.id = id
 
             # Adding user on DB
             self.db.create_user(id)
@@ -80,12 +83,13 @@ class ChatServer:
         except Exception as e:
             print("Error on register an user: ", e)
 
-    def login_user(self, addr, id, user: User):
+    def login_user(self, id, user: User):
         try:
             # Check if user exists and making user not be offline
-            if id in self.offline_users:    
+            if id in self.offline_users:
                 self.online_users[id] = user
                 del self.offline_users[id]
+                user.id = id 
                 
                 return True
             return False
@@ -99,50 +103,64 @@ class ChatServer:
             user = self.online_users[id]
             del self.online_users[id]
             self.offline_users[id] = user
+    
+    def send_message_group(self, id_group, id_sender, time, message):
+        if id_group in self.groups:
+            for member_id in self.groups[id_group].users:
+                
+                if member_id in self.online_users:
+                    self.online_users[member_id].conn.sendall(f"06{id_sender}{id_group}{time}{message}".encode("utf-8"))
+                
+                elif member_id in self.offline_users:
+                    # Inserting the message on "pending messages" table on SQLite
+                    self.db.add_pending_message(member_id, f"06{id_sender}{id_group}{time}{message}", int(time))
+
+                else:
+                    # If the user is not in the dictionaries, we warn that
+                    print(f"Error on send_group_message -> the user {member_id} does not exist.")
 
     def send_message(self, id_sender, id_receiver, time, message): 
         if id_receiver in self.online_users:
             # If the user is online, send them a message
             try:
-                error = self.online_users[id_receiver].conn.send(f"06{id_sender}{id_receiver}{time}{message}".encode("utf-8"))
+                error = self.online_users[id_receiver].conn.sendall(f"06{id_sender}{id_receiver}{time}{message}".encode("utf-8"))
                 
                 # With sendall method: None is returned on success
                 if error == None:
                     # So if the message was sent, I confirm delivery to the sender
-                    error = self.online_users[id_sender].conn.send(f"07{id_receiver}{time}".encode("utf-8"))
+                    error = self.online_users[id_sender].conn.sendall(f"07{id_receiver}{time}".encode("utf-8"))
 
             except Exception as e:
                 print("An error ocurred on send message.")
-            pass
 
         elif id_receiver in self.offline_users:
-            # If the user is not online, I create a message with the content
-            msg = Message(id_sender, id_receiver, time, message)
-            # And add it to "messages" which is an instance of the Queue we created
-            self.offline_users[id_receiver].messages.insert(msg)
+            # Inserting the message on "pending messages" table on SQLite
+            self.db.add_pending_message(id_receiver, f"06{id_sender}{id_receiver}{time}{message}", int(time))
+
         else:
-            # If the user is not in the dictionaries, we warn that it does not exist (to sender)
-            self.online_users[id_sender].conn.sendall(f"The user does not exist.".encode("utf-8"))
+            # If the user is not in the dictionaries, we warn that
+            print(f"Error on send_message -> the user {id_receiver} does not exist.")
 
-    def confirm_receipt(self, id_sender):
-        if id_sender in self.online_users:
-            self.online_users[id_sender].conn.sendall(f"Message arrived on the server!".encode("utf-8"))
-
-    def confirm_delivery(self, id_sender, id_receiver, time):
-        if id_sender in self.online_users:
-            self.online_users[id_sender].conn.sendall(f"07{id_receiver}{str(time.time())[:10]}")
-
-    def confirm_read(self, id_sender, id_receiver, time):
-        if id_sender in self.online_users:
-            self.online_users[id_sender].conn.sendall(f"08{id_receiver}{str(time.time())[:10]}")
+    def confirm_read(self, id_source, time, id_receiver):
+        if id_source in self.online_users:
+            self.online_users[id_source].conn.sendall(f"09{id_receiver}{time}")
 
     def create_group(self, id_creator, time, members):
-        group = Group(id_creator, time, members)
-        self.groups[group.id] = group
+        group = Group(self, id_creator, time, members)
+        self.db.create_group(group.id)
+
+        for member_id in members:
+            self.db.add_user_to_group(member_id, group.id)
+            
+        self.db.add_user_to_group(id_creator, group.id)
+
         for member_id in members:
             if member_id in self.online_users:
-                self.online_users[member_id].conn.sendall(f"11{group.id}{group.creation}{str(time.time())[:10]}{members}".encode("utf-8"))
-        
+                self.online_users[member_id].conn.sendall(f"11{group.id}{time}{members}".encode("utf-8"))
+            else:
+                # Inserting the message on "pending messages" table on SQLite
+                self.db.add_pending_message(member_id, f"11{group.id}{time}{members}", int(time))
+
         if id_creator in self.online_users:
-            self.online_users[id_creator].conn.sendall(f"11{group.id}{group.creation}{str(time.time())[:10]}{members}".encode("utf-8"))
+            self.online_users[id_creator].conn.sendall(f"11{group.id}{time}{members}".encode("utf-8"))
   
--- server/internal/database.py
+++ server/internal/database.py
@@ -17,6 +17,14 @@ class Database:
                 PRIMARY KEY(user_id, group_id)
             )
         """)
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS pending_messages(
+                receiver_id TEXT,
+                message TEXT,
+                timestamp INTEGER,
+                FOREIGN KEY(receiver_id) REFERENCES users(id)
+            )
+        """)
         self.conn.commit()
 
     def reset_db(self):
@@ -26,18 +34,9 @@ class Database:
         self.conn.commit()
         self.start_db()
 
+    # Users
     def get_all_users(self):
         return self.cursor.execute("SELECT * FROM users").fetchall()
-    
-    def get_all_groups(self):
-        return self.cursor.execute("SELECT * FROM groups").fetchall()
-    
-    def get_group_members(self, group_id):
-        self.cursor.execute("""
-            SELECT user_id FROM user_groups
-            WHERE group_id = ?
-        """, (group_id,))
-        return self.cursor.fetchall()
     
     def create_user(self, id):
         try:
@@ -48,6 +47,10 @@ class Database:
             print("Error on create user on DB: ", e)
             return False
     
+    # Groups
+    def get_all_groups(self):
+        return self.cursor.execute("SELECT * FROM groups").fetchall()
+    
     def create_group(self, id):
         try:
             self.cursor.execute("INSERT INTO groups VALUES(?)", (id,))
@@ -55,6 +58,13 @@ class Database:
             return True
         except Exception as e:
             return False
+    
+    def get_group_members(self, group_id):
+        self.cursor.execute("""
+            SELECT user_id FROM user_groups
+            WHERE group_id = ?
+        """, (group_id,))
+        return self.cursor.fetchall()
 
     def add_user_to_group(self, user_id, group_id):
         try:
@@ -71,3 +81,31 @@ class Database:
             WHERE user_groups.group_id = ?
         """, (group_id,))
         return self.cursor.fetchall()
+    
+    # Pending messages
+    def add_pending_message(self, receiver_id, message, time):
+        print("PENDING:",receiver_id, message, time)
+        try:
+            self.cursor.execute("""
+                INSERT INTO pending_messages (receiver_id, message, timestamp)
+                VALUES (?, ?, ?)
+            """, (receiver_id, message, int(time)))
+            self.conn.commit()
+            return True
+        except Exception as e:
+            print(f"Error on adding pending message to DB: {e}")
+            return False
+
+    def get_pending_messages(self, user_id):
+        return self.cursor.execute("""
+            SELECT message FROM pending_messages
+            WHERE receiver_id = ?
+            ORDER BY timestamp ASC
+        """, (user_id,)).fetchall()
+
+    def delete_pending_messages(self, user_id):
+        self.cursor.execute("""
+            DELETE FROM pending_messages
+            WHERE receiver_id = ?
+        """, (user_id,))
+        self.conn.commit()
