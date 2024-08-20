bf68468209b3ef5b098fec9a042e750fa4f18295
Romulo <romulotg12@gmail.com>
2024-08-20 11:34:32 -0300

    fix: using os and sys to import HOST_IP

--- client/main.py
+++ client/main.py
@@ -1,5 +1,9 @@
 from internal.chat_client import ChatClient
 
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from host import HOST_IP
 
 PORT = 9070
--- server/main.py
+++ server/main.py
@@ -1,5 +1,10 @@
 from internal.chat_server import ChatServer
 from internal.database import Database
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from host import HOST_IP
 
 PORT = 9070
